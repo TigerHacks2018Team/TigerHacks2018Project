491dd0e690b787ce94ceb62f2d1e73c27996601a
Brad Boutaugh <brad.boutaugh@gmail.com>
2018-10-13 23:50:30 -0500

    commit3 10/13/18

---
+++ public_html/js/login.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+ var config = {
+    apiKey: "AIzaSyCy-eu6UWKQDaNE31tj4Y4ePxkL2BLfN0Q",
+    authDomain: "tigerhacks2018.firebaseapp.com",
+    databaseURL: "https://tigerhacks2018.firebaseio.com",
+    projectId: "tigerhacks2018",
+    storageBucket: "tigerhacks2018.appspot.com",
+    messagingSenderId: "433043699698"
+  };
+  firebase.initializeApp(config);
